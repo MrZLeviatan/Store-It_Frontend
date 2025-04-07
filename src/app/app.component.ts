48b0c7f128f7c6d9909f4ceefb987803c170c6f0
Nicolas C.S <nikis281002@gmail.com>
2025-04-07 08:59:55 -0500

    Funciono
    Funciono

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,14 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { AuthContainerComponent } from './components/auth-container/auth-container.component';
+import {RouterOutlet} from '@angular/router';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, AuthContainerComponent],
-  template: `<app-auth-container></app-auth-container>`
+  imports: [CommonModule, AuthContainerComponent, RouterOutlet],
+  template: `<router-outlet></router-outlet>`,  // Este es el lugar donde se renderizan las rutas
 })
 export class AppComponent {}
 
--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import {Routes} from '@angular/router';
 import { RegistrarClienteComponent } from './cliente/registro-cliente/form/registroCliente-form.component';
 import {ClientePerfilComponent} from './cliente/informacion-cliente/form/cliente-perfil.components';
+import {AuthContainerComponent} from './components/auth-container/auth-container.component';
 
 export const routes: Routes = [
+  { path: '', component: AuthContainerComponent },
   { path: '', component: RegistrarClienteComponent }, // ✅ Página principal
   { path: 'cliente/perfil/:id', component: ClientePerfilComponent}, // Ruta con el parámetro ID
 ];
